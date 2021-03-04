1e25d2ed0fd922964f948683405de9a9d7775982
Yilun <michaelxie.yl@gmail.com>
2021-03-03 21:55:39 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import yfinance as yf
+
+# Specify Dataframe and download past months figures
+nvda_df = yf.download('NVDA',
+                      start='2021-01-01',
+                      end='2021-01-27',
+                      progress=False)
+
+# View the first fiew records
+nvda_df.head()
