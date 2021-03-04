92990ab9fc838ea76b3ef9518abc0c5c4526894c
Yilun <michaelxie.yl@gmail.com>
2021-03-04 22:13:32 +0000

    multi-companies support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import click
 import pandas as pd
 import yfinance as yf
+from yahoofinancials import YahooFinancials
 from datetime import date, timedelta
 
 @click.command()
@@ -22,6 +23,26 @@ def show_data(name):
 # View the first fiew records
     print(nvda_df)
 
+def get_data():
+    COMPANIES = ['NVDA', 'GME']
+
+    # Set Object
+    yfs = YahooFinancials(COMPANIES)
+
+    # Set date range
+    today = date.today()
+    dta = timedelta(days=365)
+    prev = today - dta
+    d1 = today.strftime("%Y-%m-%d")
+    d2 = prev.strftime("%Y-%m-%d")
+
+    # maket the call
+    data = yfs.get_historical_price_data(start_date=d2, 
+                                                    end_date=d1, 
+                                                    time_interval='monthly')
+    print(data['NVDA']['prices'])
+
 if __name__ == '__main__':
     #pylint: disable=no-value-for-parameter
-    show_data()
+    #show_data()
+    get_data()
