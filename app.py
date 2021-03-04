30466a2b0c43334e23eb745afc09db4a2348e092
Yilun <michaelxie.yl@gmail.com>
2021-03-04 23:23:38 +0000

    add expected return calc for the portfolio

--- app.py
+++ app.py
@@ -15,34 +15,53 @@ def show_data(name):
     d1 = today.strftime("%Y-%m-%d")
     d2 = prev.strftime("%Y-%m-%d") 
     # Specify Dataframe and download past months figures
-    nvda_df = yf.download(name,
+    nvda_df = yf.download("^TNX",
                       start=d2,
                       end=d1,
                       progress=False)
 
-# View the first fiew records
+    # View the first fiew records
     print(nvda_df)
 
 def get_data():
-    COMPANIES = ['NVDA', 'GME']
+    COMPANIES = ['NVDA', 'GME', '^TNX', '^IXIC']
+    portforlio_cmp = {
+                "GME":{"num_shares":10, "value":0.0, "weight":0.0, "weighted_beta":0.0},
+                "NVDA":{"num_shares":5, "value":0.0, "weight":0.0, "weighted_beta":0.0}
+                    }
 
     # Set Object
     yfs = YahooFinancials(COMPANIES)
 
     # Set date range
     today = date.today()
-    dta = timedelta(days=365)
+    dta = timedelta(days=2)
     prev = today - dta
     d1 = today.strftime("%Y-%m-%d")
     d2 = prev.strftime("%Y-%m-%d")
 
     # maket the call
     data = yfs.get_historical_price_data(start_date=d2, 
-                                                    end_date=d1, 
-                                                    time_interval='monthly')
-    print(data['NVDA']['prices'])
+                                         end_date=d1, 
+                                         time_interval='monthly')
+    stats_data = yfs.get_key_statistics_data()
+
+    total_asset_price = 0.0
+    for stock in portforlio_cmp:
+        portforlio_cmp[stock]["value"] = float(data[stock]['prices'][-1]['close']) * portforlio_cmp[stock]["num_shares"]
+        total_asset_price += portforlio_cmp[stock]["value"]
+    # update the weight
+    portforlio_beta = 0.0
+    for stock in portforlio_cmp:
+        portforlio_cmp[stock]["weight"] = float(data[stock]['prices'][-1]['close']) * portforlio_cmp[stock]["num_shares"] / total_asset_price
+        portforlio_cmp[stock]["weighted_beta"] = portforlio_cmp[stock]["weight"] * float(stats_data[stock]['beta'])
+        portforlio_beta += portforlio_cmp[stock]["weighted_beta"]
+
+        
+    portfolio_expected_return = float(data['^TNX']['prices'][-1]['close'])/100 + portforlio_beta * (0.1 - float(data['^TNX']['prices'][-1]['close'])/100)
+    print(portfolio_expected_return)
+    
 
 if __name__ == '__main__':
-    #pylint: disable=no-value-for-parameter
-    #show_data()
+    # show_data()
     get_data()
