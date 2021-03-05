8181d4072589e373ebd6195ead289b6f4392dafa
yilun306 <50850981+yilun306@users.noreply.github.com>
2021-03-05 17:31:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
+import click
+import pandas as pd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 from datetime import date, timedelta
 
+@click.command()
+@click.option('--name', prompt='Enter Stock Name',
+              help='The stock you want to check.')
+
+def show_data(name):
+    today = date.today()
+    dta = timedelta(days=30)
+    prev = today - dta
+    d1 = today.strftime("%Y-%m-%d")
+    d2 = prev.strftime("%Y-%m-%d") 
+    # Specify Dataframe and download past months figures
+    nvda_df = yf.download(name,
+                      start=d2,
+                      end=d1,
+                      progress=False)
+
+    # View the first fiew records
+    print(nvda_df)
 
 def get_data():
     COMPANIES = ['FMC', 'GME', 'PTON', 'U']
